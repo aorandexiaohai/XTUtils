1678a641f0be3ba0d81acba3b54239a572615e85
wuzhenhai <zhenhai.wu@youhualin.com>
2018-08-01 11:07:23 +0800

    add DEBUG_CODE_OUTPUT macro

--- xt_debug_helper.h
+++ xt_debug_helper.h
@@ -1,10 +1,19 @@
 #pragma once
 #if defined(DEBUG) || defined(_DEBUG)
 #define DEBUG_HELPER(expression) expression
-#define DEBUG_PAUSE  FunctionOverGuard<> fog_25AB818D_2DF9_4419_9B9A_4D3BB3E7A391{[](){std::cout << "debug pause"<<std::endl; std::cin.clear(); std::cin.get();}};
+#define DEBUG_PAUSE  FunctionOverGuard<> fog_25AB818D_2DF9_4419_9B9A_4D3BB3E7A391{[](){std::cout << "\ndebug pause\n"; std::cin.clear(); std::cin.get();}};
+
+#define DEBUG_CODE_OUTPUT(expression)\
+std::cout<<"source code:\n"\
+<<#expression<<"\n"\
+<<"begin:\n\n";\
+expression \
+std::cout << "\nfinished\n";
+
 #else
 #define DEBUG_HELPER(expression)
 #define DEBUG_PAUSE 
+#define DEBUG_CODE_OUTPUT(expression) expression
 #endif
 
-#define FORCE_PAUSE  FunctionOverGuard<> fog_25AB818D_2DF9_4419_9B7A_4D3BB3E7A391{[](){std::cout << "force pause"<<std::endl; std::cin.clear(); std::cin.get();}};
+#define FORCE_PAUSE  FunctionOverGuard<> fog_25AB818D_2DF9_4419_9B7A_4D3BB3E7A391{[](){std::cout << "\nforce pause\n"; std::cin.clear(); std::cin.get();}};
