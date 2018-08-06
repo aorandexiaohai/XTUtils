d004359597252036a0054449e72a94c255b48dd2
wuzhenhai <zhenhai.wu@youhualin.com>
2018-08-06 16:23:19 +0800

    new pause way

--- xt_debug_helper.h
+++ xt_debug_helper.h
@@ -1,7 +1,7 @@
 #pragma once
 #if defined(DEBUG) || defined(_DEBUG)
 #define DEBUG_HELPER(expression) expression
-#define DEBUG_PAUSE  FunctionOverGuard<> fog_25AB818D_2DF9_4419_9B9A_4D3BB3E7A391{[](){std::cout << "\ndebug pause\n"; std::cin.clear(); std::cin.get();}};
+#define DEBUG_PAUSE  FunctionOverGuard<> fog_25AB818D_2DF9_4419_9B9A_4D3BB3E7A391{[](){std::cout << "\ndebug pause\n"; std::cin.ignore(); std::cin.get();}};
 
 #define DEBUG_CODE_OUTPUT(expression)\
 std::cout<<"source code:\n"\
@@ -16,4 +16,4 @@ std::cout << "\nfinished\n";
 #define DEBUG_CODE_OUTPUT(expression) expression
 #endif
 
-#define FORCE_PAUSE  FunctionOverGuard<> fog_25AB818D_2DF9_4419_9B7A_4D3BB3E7A391{[](){std::cout << "\nforce pause\n"; std::cin.clear(); std::cin.get();}};
+#define FORCE_PAUSE  FunctionOverGuard<> fog_25AB818D_2DF9_4419_9B7A_4D3BB3E7A391{[](){std::cout << "\nforce pause\n"; std::cin.ignore(); std::cin.get();}};
