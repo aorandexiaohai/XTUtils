cdd3a2492c696f704badaade14a09e77db5f0714
wuzhenhai <2663976081@qq.com>
2018-11-16 02:09:40 -0800

    code

---
+++ xt_asyc_cout.cpp
@@ -0,0 +1,2 @@
+#include "xt_asyc_cout.h"
+std::mutex AsyncCout::cout_mutex{};
--- xt_asyc_cout.h
+++ xt_asyc_cout.h
@@ -1,16 +1,11 @@
 #pragma once
 #include "xt_std_head.h"
-//struct AsyncCout : public std::stringstream {
-//	static std::mutex cout_mutex;
-//	~AsyncCout() {
-//		std::lock_guard<std::mutex> l{ cout_mutex };
-//		std::cout << rdbuf();
-//		std::cout.flush();
-//	}
-//};
-//
-//std::mutex AsyncCout::cout_mutex{};
-
-//#define ACOUT AsyncCout{}
-
-#define ACOUT std::cout
+struct AsyncCout : public std::stringstream {
+	static std::mutex cout_mutex;
+	~AsyncCout() {
+		std::lock_guard<std::mutex> l{ cout_mutex };
+		std::cout << rdbuf();
+		std::cout.flush();
+	}
+};
+#define ACOUT AsyncCout{}
